e168cd5d077865dc60e06441d5c73bc8d6d9a5fb
Mawande06 <mawande@projectcodex.co>
2014-12-09 15:21:06 +0200

    process.env

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app); 
-//var io = require('socket.io')(http); 
-
-
 
 
 app.use(express.static(__dirname + '/public'));
@@ -13,56 +10,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 }); 
 
-/*io.on('connection', function(socket){
-  console.log('Ready to twist!'); 
-
-var varCounter = 0; 
-
-
-setInterval(function(){ 
-varCounter += 10;
-io.emit('twist', { counter: varCounter}); 
-}, 2000);
-*/
-
-/*
-var five = require("johnny-five"),board, myPotentiometer;
-board = new five.Board();
-board.on("ready", function() {
-	myPotentiometer = new five.Sensor({
-	pin: "A0",
-	freq: 250
-});
-
-myLed = new five.Led(9);
-
-myPotentiometer.on("read", function() { 
-		varCounter = this.raw;
-		io.emit('twist', { counter: varCounter});
-});
-	
-
-});
-
-*/
-
-
-
-//}); 
 
  
+var port = process.env.soccer_viz_port || 3000;
 
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-}); 
-
-/*
-document.ready(function(){
-setTimeout(function() {console.log("It works!!")}, 5000);
-}); 
-*/
- 
+http.listen( port, function(){
+  console.log('listening on *:' + port);
+});
 
 
